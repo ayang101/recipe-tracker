f58fee5da464797bb877b6eb25455bba20b98c22
yang, anna <annayang877@gmail.com>
2023-03-23 16:07:47 -0700

    added ingredient model and services

---
+++ expressjs-backend/models/ingredient-services.js
@@ -0,0 +1,69 @@
+const connectMongoDB = require('./mongoose.db.config');
+const ingredientModel = require('./ingredient');
+
+connectMongoDB();
+
+async function getIngredients(name, photo, description, quantity) {
+  let result;
+  if (
+    name === undefined &&
+    photo === undefined &&
+    description === undefined &&
+    quantity === undefined
+  ) {
+    result = await ingredientModel.find();
+  } else if (name && !photo && !description && !quantity) {
+    result = await findIngredientByName(name);
+  } else if (photo && !name && !description && !quantity) {
+    result = await findIngredientByPhoto(photo);
+  } else if (description && !name && !photo && !quantity) {
+    result = await findIngredientByDescription(description);
+  } else {
+    result = await findIngredientByQuantity(quantity);
+  }
+  return result;
+}
+
+async function findIngredientById(id) {
+    return await ingredientModel.findById(id);
+}
+
+async function addIngredient(ingredient) {
+  try {
+    const ingredientToAdd = new ingredientModel(ingredient);
+    const savedIngredient = await ingredientToAdd.save();
+    return savedIngredient;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+}
+
+async function findIngredientByName(name) {
+  return await ingredientModel.find({ name: name });
+}
+
+async function findIngredientByPhoto(photo) {
+  return await ingredientModel.find({ photo: photo });
+}
+
+async function findIngredientByDescription(description) {
+  return await ingredientModel.find({ description: description });
+}
+
+async function findIngredientByQuantity(quantity) {
+  return await ingredientModel.find({ quantity: quantity });
+}
+
+async function deleteIngredient(id) {
+  return await ingredientModel.findByIdAndDelete(id);
+}
+
+exports.getIngredients = getIngredients;
+exports.findIngredientById = findIngredientById;
+exports.findIngredientByName = findIngredientByName;
+exports.findIngredientByPhoto = findIngredientByPhoto;
+exports.findIngredientByDescription = findIngredientByDescription;
+exports.findIngredientByQuantity = findIngredientByQuantity;
+exports.addIngredient = addIngredient;
+exports.deleteIngredient = deleteIngredient;
---
+++ expressjs-backend/models/ingredient.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const IngredientSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    photo: {
+      type: String,
+      required: false,
+      trim: true,
+      // need to validate image source
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    quantity: {
+      type: Number,
+      required: false,
+      trim: true
+    }
+  },
+  { collection: 'ingredient_list' }
+);
+
+const Ingredient = mongoose.model('Ingredient', IngredientSchema);
+
+module.exports = Ingredient;
