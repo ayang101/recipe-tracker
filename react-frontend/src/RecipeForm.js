5f111278c6e77ff1e599b6d18e863e2fe6916291
yang, anna <annayang877@gmail.com>
2023-06-14 08:05:56 -0700

    removed/added unecessary/relevant sections

--- react-frontend/src/RecipeForm.js
+++ react-frontend/src/RecipeForm.js
@@ -226,13 +226,6 @@ function RecipeForm(props) {
 
     return (
         <form>
-            <label htmlFor="name">Name</label>
-            <input
-                type="text"
-                name="name"
-                id="name"
-                value={recipe.name}
-                onChange={handleChange} />
             <label htmlFor="source">Source</label>
             <input
                 type="text"
@@ -240,85 +233,6 @@ function RecipeForm(props) {
                 id="source"
                 value={recipe.source}
                 onChange={handleChange} />
-{/*
-            <label htmlFor="image">Image</label>
-            <input
-                type="text"
-                name="image"
-                id="image"
-                value={recipe.image}
-                onChange={handleChange} />
-            <label htmlFor="rating">Rating</label>
-            <input
-                type="text"
-                name="rating"
-                id="rating"
-                value={recipe.rating}
-                onChange={handleChange} />
-            <label htmlFor="course">Course</label>
-            <input
-                type="text"
-                name="course"
-                id="course"
-                value={recipe.course}
-                onChange={handleChange} />
-            <label htmlFor="category">Category</label>
-            <input
-                type="text"
-                name="category"
-                id="category"
-                value={recipe.category}
-                onChange={handleChange} />
-            <label htmlFor="servingSize">Serving Size</label>
-            <input
-                type="text"
-                name="servingSize"
-                id="servingSize"
-                value={recipe.servingSize}
-                onChange={handleChange} />
-            <label htmlFor="prepTime">Prep Time</label>
-            <input
-                type="text"
-                name="prepTime"
-                id="prepTime"
-                value={recipe.prepTime}
-                onChange={handleChange} />
-            <label htmlFor="cookTime">Cook Time</label>
-            <input
-                type="text"
-                name="cookTime"
-                id="cookTime"
-                value={recipe.cookTime}
-                onChange={handleChange} />
-            <label htmlFor="totalTime">Total Time</label>
-            <input
-                type="text"
-                name="totalTime"
-                id="totalTime"
-                value={recipe.totalTime}
-                onChange={handleChange} />
-            <label htmlFor="description">Description</label>
-            <input
-                type="text"
-                name="description"
-                id="description"
-                value={recipe.description}
-                onChange={handleChange} />
-            <label htmlFor="ingredient_list">Ingredients</label>
-            <input
-                type="text"
-                name="ingredient_list"
-                id="ingredient_list"
-                value={recipe.ingredient_list}
-                onChange={handleChange} />
-            <label htmlFor="instructions">Instructions</label>
-            <input
-                type="text"
-                name="instructions"
-                id="instructions"
-                value={recipe.instructions}
-                onChange={handleChange} />
-    */}
             <input type="button" value= "Submit" onClick={submitForm} />
         </form>
     );
