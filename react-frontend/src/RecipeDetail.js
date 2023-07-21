b83aa1541d03cedeb0516d0ca5947ec6682e73bd
yang, anna <annayang877@gmail.com>
2023-07-21 12:47:44 -0700

    fixed bug: used useRef to render upon page refresh

--- react-frontend/src/RecipeDetail.js
+++ react-frontend/src/RecipeDetail.js
@@ -1,37 +1,43 @@
 import React from 'react'
+import { useRef } from 'react';
 import { useParams } from 'react-router-dom'
 
 
 function RecipeDetail(props) {
+    const savedRecipe = useRef(0);
     const recipeId = useParams().id;
     const currRecipe = props.recipeData.find(rec => {return rec._id === recipeId});
+    if (currRecipe !== undefined) {
+        savedRecipe.current = currRecipe;
+    }
 
     return (
         <div>
-            <h1>{currRecipe.name}</h1>
+            <h1>{savedRecipe.current.name}</h1>
+            <p>Author: {savedRecipe.current.author}</p>
             <p>Source:
-                <a href= {currRecipe.source} target="_blank" rel="noopener noreferrer">
-                    {currRecipe.source}
+                <a href= {savedRecipe.current.source} target="_blank" rel="noopener noreferrer">
+                    {savedRecipe.current.source}
                 </a>
             </p>
             <p>Image:</p>
-            <p><a href= {currRecipe.source} target="_blank" rel="noopener noreferrer">
-                <img src={currRecipe.image} 
+            <p>
+                <img src={savedRecipe.current.image} 
                     alt=''
                     width="50%"/>
-            </a></p>
-            <p>Rating: {currRecipe.rating}</p>
-            <p>Course: {currRecipe.course}</p>
-            <p>Cuisine: {currRecipe.cuisine}</p>
-            <p>Servings: {currRecipe.servingSize}</p>
-            <p>Prep Time: {currRecipe.prepTime}</p>
-            <p>Cook Time: {currRecipe.cookTime}</p>
-            <p>Additional Time: {currRecipe.additionalTime}</p>
-            <p>Total Time: {currRecipe.totalTime}</p>
-            <p>Description: {currRecipe.description}</p>
+            </p>
+            <p>Rating: {savedRecipe.current.rating}</p>
+            <p>Course: {savedRecipe.current.course}</p>
+            <p>Cuisine: {savedRecipe.current.cuisine}</p>
+            <p>Servings: {savedRecipe.current.servingSize}</p>
+            <p>Prep Time: {savedRecipe.current.prepTime}</p>
+            <p>Cook Time: {savedRecipe.current.cookTime}</p>
+            <p>Additional Time: {savedRecipe.current.additionalTime}</p>
+            <p>Total Time: {savedRecipe.current.totalTime}</p>
+            <p>Description: {savedRecipe.current.description}</p>
             <p>Ingredients: </p>
             <ul>
-                {currRecipe.ingredients.map((element, index) => {
+                {savedRecipe.current.ingredients?.map((element, index) => {
                     return(
                         <li key={index}>
                             {element}
@@ -41,7 +47,7 @@ function RecipeDetail(props) {
             </ul>
             <p>Instructions:</p>
             <ol>
-                {currRecipe.instructions.map((element, index) => {
+                {savedRecipe.current.instructions?.map((element, index) => {
                     return(
                         <li key={index}>
                             {element}
