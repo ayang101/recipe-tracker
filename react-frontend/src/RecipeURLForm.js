987f43e4d84290f13bf0dee8959f93e04b641686
yang, anna <annayang877@gmail.com>
2023-07-22 18:42:28 -0700

    added styling to recipe detail and nav bar

--- react-frontend/src/RecipeURLForm.js
+++ react-frontend/src/RecipeURLForm.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import './App.css';
 
 function RecipeURLForm(props) {
     const [isValidURL, setIsValidURL] = useState(true);
@@ -69,7 +70,7 @@ function RecipeURLForm(props) {
     }
 
     return (
-        <form>
+        <form className='recipe-form'>
             <label htmlFor="source">Source</label>
             <input
                 type="text"
