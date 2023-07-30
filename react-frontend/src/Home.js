b18fdd36969dc5d995c736c073e9880f45fee19f
yang, anna <annayang877@gmail.com>
2023-07-30 13:49:40 -0700

    added logout page

--- react-frontend/src/Home.js
+++ react-frontend/src/Home.js
@@ -1,5 +1,23 @@
+import { Link } from "react-router-dom";
+import './App.css';
+
+
 const Home = () => {
-    return <h1>Recipe Tracker</h1>;
+    return (
+        <div className='home'>
+            <div className='header'>
+                <ul>
+                    <li><Link to='/signup'>Sign Up</Link></li>
+                    <li><Link to='/login'>Login In</Link></li>
+                    <li><Link to='/logout'>Log Out</Link></li>
+                </ul>
+            </div>
+            <div>
+                <h1 className="app-name">Recipe Tracker</h1>
+            </div>
+        </div>
+    );
+
 };
 
 export default Home;
