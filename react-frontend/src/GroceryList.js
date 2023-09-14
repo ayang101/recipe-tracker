d6e0fc8aeac25bb920f3168ccdcc8db0a52e82fa
yang, anna <annayang877@gmail.com>
2023-09-13 18:29:06 -0700

    adjusted for empty list

--- react-frontend/src/GroceryList.js
+++ react-frontend/src/GroceryList.js
@@ -5,13 +5,16 @@ function GroceryList(props) {
         <div className='grocery-list'>
             <h1>Grocery List</h1>
             <ul>
-                {localStorage.getItem("savedIngr").toString().replace(/\[|\]/g,'').split(',')?.map((element, index) => {
-                    return(
-                        <li key={index}>
-                            {element}
-                        </li>
-                    );
-                })}
+                {localStorage.getItem("savedIngr") ?
+                    localStorage.getItem("savedIngr").toString().replace(/\[|\]/g,'').split(',')?.map((element, index) => {
+                        return(
+                            <li key={index}>
+                                {element}
+                            </li>
+                        );
+                    }) :
+                    <h4>No items so far</h4>
+                }
             </ul>
         </div>
     );
